e3e6ebb20c19cce773da89abef722a053af1034d
josecantu_20@yahoo.com <josecantu_20@yahoo.com>
2021-11-05 13:19:01 -0500

    user Auth Done, test routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,26 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      },
+    })
+  setTokenCookie(res, user);
+  return res.json({ user });
+}));
+
+const { restoreUser } = require('../../utils/auth.js');
+router.get(
+  '/restore-user',
+  restoreUser,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
 
 router.post('/test', function(req,res) {
     res.json({requestsBody: req.body});
