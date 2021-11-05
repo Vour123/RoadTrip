4370827c4f0be9b79b2781b80e932ddc67e690bc
josecantu_20@yahoo.com <josecantu_20@yahoo.com>
2021-11-05 02:08:35 -0500

    phase 2 done

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
@@ -34,6 +35,34 @@ if (!isProduction) {
       },
     })
   );
+  
+  app.use(routes);
 
-app.use(routes);
+  app.use((err, req, res, next) => {
+    if (err instanceof ValidationError) {
+      err.errors = err.errors.map((e) => e.message);
+      err.title = "Validation Error";
+    }
+    next(err);
+  });
+
+  app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+  });
+
+  app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack,
+    });
+  });
+
 module.exports = app;
