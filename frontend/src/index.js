72ade280f61c88ca5afa30c5db6cd0d7e31d2fcb
josecantu_20@yahoo.com <josecantu_20@yahoo.com>
2021-11-07 02:27:59 -0600

    session store;

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
@@ -15,6 +15,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
