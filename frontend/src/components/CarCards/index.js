ee8a734685943f281a6b93d89645db8715de37bc
josecantu_20@yahoo.com <josecantu_20@yahoo.com>
2021-11-09 12:44:26 -0600

    getAllCarRendering

--- frontend/src/components/CarCards/index.js
+++ frontend/src/components/CarCards/index.js
@@ -2,11 +2,11 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllCars } from '../../store/cars';
+import CarCardDetails from '../CarCardDetails/index';
 
 const Cars = () => {
     const cars = useSelector((state) => Object.values(state.car));
     const dispatch = useDispatch();
-
     console.log('cars', cars);
 
     useEffect(() => {
@@ -15,8 +15,17 @@ const Cars = () => {
 
     return (
         <div>
-            <div className='cars-container'>
-
+            <div className='cars-cards-container'>
+                {cars.length > 0 ? cars.map(({id, image, price, name}) => {
+                    <div>hi</div>
+                    // <CarCardDetails
+                    // key={id}
+                    // id={id}
+                    // image={image}
+                    // name={name}
+                    // price={price}
+                    // />
+                }): null}
             </div>
         </div>
     );
