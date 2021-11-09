aa723930c5f172a855310befe17b40cb1b08ca19
josecantu_20@yahoo.com <josecantu_20@yahoo.com>
2021-11-09 15:03:10 -0600

    seeded car

--- backend/db/seeders/20211109023115-create-listings.js
+++ backend/db/seeders/20211109023115-create-listings.js
@@ -12,6 +12,15 @@ module.exports = {
         name: "Audi RS3",
         tags: "Audi",
         price: 50
+      },
+      {
+        userId: 1,
+        city: "McAllen",
+        state: "Texas",
+        country: "USA",
+        name: "Audi RS5",
+        tags: "Audi",
+        price: 100
       }
     ],
     {}
